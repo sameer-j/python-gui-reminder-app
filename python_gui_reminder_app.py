02decdfb8de541705f2cd6a2bf4fd2825b49a7e3
Rinku Kumari <rinkuk1993@gmail.com>
2020-10-03 10:12:14 +0530

    generalized reminder for any title and message (#1)

--- python_gui_reminder_app.py
+++ python_gui_reminder_app.py
@@ -1,14 +1,13 @@
 """
 author: 'Sameer Jain'
+updated by: 'Rinku Kumari'
 
 -------------------------
-Reminder App V2
+Reminder App V3
 -------------------------
 Python GUI app using tkinter.
 V2 makes use of custom message box to show the pop up message.
 program needs to be run from command line/ terminal.
-
-Note: Right now works as eye exercise reminder.
 """
 
 from time import sleep
@@ -17,7 +16,7 @@ from tkinter import ttk
 
 execution_count = 0
 
-def eye_action(win, more):
+def action(win, more):
     global execution_count
     global root
     print('Answer', more)
@@ -25,12 +24,12 @@ def eye_action(win, more):
         win.destroy()
         sleep(snooze_time)
         execution_count = execution_count + 1
-        EyeReminderWindow()
+        ReminderWindow(title, message)
     else:
         win.destroy()
         root.destroy()
 
-def EyeReminderWindow():
+def ReminderWindow(title, message):
     global root
     print('Execution', execution_count)
     win = Toplevel()
@@ -40,28 +39,30 @@ def EyeReminderWindow():
     y = (win.winfo_screenheight() - win.winfo_reqheight()) / 2
     win.geometry("+%d+%d" % (x, y))
     win.deiconify()
-    win.title('Eye Exercise Reminder')
-    message1='Time for Eye Exercise!'
+    win.title(title)
+    message1=message
     message2='Current Snooze time={0} seconds'.format(snooze_time)
     message3 = 'Do you want more reminders?'
     ttk.Label(win, text=message1).grid(column=0, row=0)
     ttk.Label(win, text=message2).grid(column=0, row=1)
     ttk.Label(win, text=message3).grid(column=0, row=2)
-    yes_btn = ttk.Button(win, text='Yes', command=lambda: eye_action(win, True))
+    yes_btn = ttk.Button(win, text='Yes', command=lambda: action(win, True))
     yes_btn.grid(column=0,row=3)
-    ttk.Button(win, text='No', command=lambda: eye_action(win, False)).grid(column=1, row=3)
+    ttk.Button(win, text='No', command=lambda: action(win, False)).grid(column=1, row=3)
     yes_btn.focus()
     win.lift()
     win.attributes('-topmost', True)
 
 print('\n\n\n')
-print('Welcome to Reminder App(beta v2)!')
+print('Welcome to Reminder App(beta v3)!')
 print('-------------------------------------------------')
 print('Once started, the app runs indefinetly till you ask it stop.')
-print('It will pop up a message window every set snooze interval time to remind you to do eye exercise.')
+print('It will pop up a message window every set snooze interval time to remind you to do a task periodically')
 print('-------------')
-# snooze_time = int(input('Enter Snooze interval:'))
-snooze_time = 3
+snooze_time = int(input('Enter Snooze interval:'))
+title = input('Enter title for reminder window: ')
+message = input('Enter message for reminder window: ')
+# snooze_time = 3
 print('\n\nThanks! You will get your first reminder in {0} seconds'.format(snooze_time))
 print('\n\n')
 print('App started....')
@@ -69,6 +70,6 @@ print('App started....')
 root = Tk()
 root.withdraw()
 execution_count = 1
-EyeReminderWindow()
+ReminderWindow(title, message)   # example title='Eye Exercise Reminder', message='Time for Eye Exercise!'
 root.mainloop()
 print('Exiting, bye')
